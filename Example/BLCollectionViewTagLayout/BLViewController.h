aa000f6a8846fb0b07900d7b44b058c8d426177b
Bluelich <bluelich@qq.com>
2019-09-26 22:32:58 +0800

    update demo for lib

--- Example/BLCollectionViewTagLayout/BLViewController.h
+++ Example/BLCollectionViewTagLayout/BLViewController.h
@@ -8,6 +8,6 @@
 
 @import UIKit;
 
-@interface BLViewController : UIViewController
+@interface BLViewController : UICollectionViewController
 
 @end
--- Example/BLCollectionViewTagLayout/BLViewController.m
+++ Example/BLCollectionViewTagLayout/BLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BLViewController.h"
+#import <BLCollectionViewTagLayout.h>
 
 @interface BLViewController ()
 
@@ -14,16 +15,81 @@
 
 @implementation BLViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.clearsSelectionOnViewWillAppear = NO;
+    self.collectionView.contentInset = UIEdgeInsetsMake(44, 44, 44, 44);
+    self.collectionView.backgroundColor = UIColor.redColor;
+    self.view.backgroundColor = UIColor.blueColor;
+    self.collectionView.scrollIndicatorInsets = UIEdgeInsetsMake(14, 0, 14, 0);
+    [(BLCollectionViewTagLayout *)self.collectionViewLayout autoConfigSystemAdditionalAdjustedContentInsetWith:UIApplication.sharedApplication.statusBarFrame navigationBar:self.navigationController.navigationBar tabbar:self.tabBarController.tabBar];
+    [self.collectionView registerClass:UICollectionReusableView.class
+            forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:UICollectionElementKindSectionHeader];
+    [self.collectionView registerClass:UICollectionReusableView.class
+            forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:UICollectionElementKindSectionFooter];
+    self.collectionView.contentOffset = CGPointZero;
+}
+- (void)flashScrollIndicators
+{
+    [self.collectionView flashScrollIndicators];
+}
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 10;
 }
 
-- (void)didReceiveMemoryWarning
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 10;
+}
+
+static NSInteger const labelTag = 1000;
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    UILabel *label = [cell.contentView viewWithTag:labelTag];
+    if (!label) {
+        label = [UILabel new];
+        label.tag = labelTag;
+        label.numberOfLines = 0;
+        [cell.contentView addSubview:label];
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+        [label.centerXAnchor constraintEqualToAnchor:cell.contentView.centerXAnchor].active = YES;
+        [label.centerYAnchor constraintEqualToAnchor:cell.contentView.centerYAnchor].active = YES;
+    }
+    label.text = [NSString stringWithFormat:@"section:%ld\nitem:%ld",indexPath.section,indexPath.item];
+    cell.backgroundColor = UIColor.lightGrayColor;
+    return cell;
+}
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UICollectionReusableView *view = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:kind forIndexPath:indexPath];
+    UILabel *label = [view viewWithTag:1000];
+    if (!label) {
+        label = [UILabel new];
+        label.tag = 1000;
+        label.numberOfLines = 0;
+        [view addSubview:label];
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+        [label.leftAnchor constraintEqualToAnchor:view.leftAnchor].active = YES;
+        [label.centerYAnchor constraintEqualToAnchor:view.centerYAnchor].active = YES;
+    }
+    NSString *string = [kind stringByReplacingOccurrencesOfString:@"UICollectionElementKindSection" withString:@""];
+    label.text = [NSString stringWithFormat:@"%@\nsection:%ld",string,indexPath.section];
+    view.backgroundColor = UIColor.greenColor;
+    if (kind == UICollectionElementKindSectionHeader) {
+        view.backgroundColor = UIColor.brownColor;
+    }
+    return view;
+}
+#pragma mark UICollectionViewDelegateTagStyleLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(BLCollectionViewTagLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(80 + indexPath.item * (indexPath.section + 5), collectionViewLayout.itemSize.height);
+}
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(BLCollectionViewTagLayout *)collectionViewLayout insetForSection:(NSInteger)section
+{
+    return UIEdgeInsetsMake(50, section, 50, section);
 }
 
 @end
